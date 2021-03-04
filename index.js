5dd1e566fd3d0a426d351ae841b696b4c45b7017
oopanpan <oopanpan@gmail.com>
2021-03-04 12:01:04 -0800

    commit tree fc5b986c718c776fb5559cfc46582f6d958584f8

--- index.js
+++ index.js
@@ -4,12 +4,25 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callBack) {
+      for (let e in collection){
+        callBack(collection[e])
+      }
+      return collection
     },
 
-    map: function() {
+    map: function(collection, callBack) {
+      //* not modifying the original
+      //* 
+      let result = [];
+      if (!(collection instanceof Array)){
+        result = Object.values(colletion)
+      }
 
+      for (let element in collection){
+        emptyThing.push(callBack(collection[element]))
+      }
+      return result
     },
 
     reduce: function() {
